b614a56191fb8d5829933af32a3ef1b099f62d68
Nick <nelom3@gmail.com>
2021-09-22 11:32:18 -0700

    adds fields_for

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    
+    book = Book.create(book_params)
+    redirect_to book_path(book) 
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
   end
 
   def book_params
-
+    params.require(:book).permit(:title, :author, :description, :genre_id)
   end
 end
