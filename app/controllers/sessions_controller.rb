d6ea728220bf2cd6290cddee334d3831b5a92f92
Nick <nelom3@gmail.com>
2021-09-29 11:24:56 -0700

    add authentication, moves links to layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   def welcome
   end
 
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      # add flash
+      redirect_to login_path 
+    end
+  end
+
   def destroy
     session.delete :user_id
     redirect_to root_path
