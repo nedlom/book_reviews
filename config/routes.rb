5e1d43e851172f71a24acecae36d33c4e70e03e7
Nick <nelom3@gmail.com>
2021-09-29 08:36:05 -0700

    add routes and links for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   root "sessions#welcome"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/signup", to: "users#new
+  post "/signup", to: "users#create"
+
   resources :users
   resources :genres
   resources :books
