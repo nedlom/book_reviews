059ba30f9f23bfd5b7d588210e6487c503bcbae3
Nick <nelom3@gmail.com>
2021-09-30 09:37:35 -0700

    add scope methods and omniauth gems

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,11 +3,28 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+
+  # scope methods in two ways 
+  # class level methods
+  # change the scope of collections (random -> ord, all -> some) 
+  # call in controller or view to change display
+  def self.alpha
+    order(title: :asc)
+  end
+
+  # avg rating
+  # Book.joins(:reviews).group(:id).order('avg(ratings) desc')
+  scope -> {}
+
   validates :title, presence: true, uniqueness: true
   def genre_attributes=(attributes)
     if !attributes[:name].empty?
       self.genre = Genre.find_or_create_by(name: attributes[:name].titleize)
     end
+  end
+
+  def title_and_author
+    "#{self.title} - #{self.author}"
   end
 
    
