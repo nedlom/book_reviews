81ad048a5693e7d5d7051485c26488e6160764b9
Nick <nelom3@gmail.com>
2021-10-05 10:44:11 -0700

    add helper review_message to account for number of reviews

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def review_message
+    if current_user.reviews.empty?
+      content_tag(:b, "You have not reviewed any books")
+    else
+      content_tag(:b, "Here are the books you have reviewed: ")
+    end
+  end
+  
 end
